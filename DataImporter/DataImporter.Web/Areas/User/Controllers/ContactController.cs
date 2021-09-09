3b3f86541391308147c374edde9eb51a0fa04bd3
zahid <zahidsheikhuap96@gmail.com>
2021-09-09 21:18:40 +0600

    Design of Contact List completed

--- DataImporter/DataImporter.Web/Areas/User/Controllers/ContactController.cs
+++ DataImporter/DataImporter.Web/Areas/User/Controllers/ContactController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataImporter.Web.Areas.User.Controllers
 {
+    [Area("User")]
     public class ContactController : Controller
     {
         public IActionResult Index()
